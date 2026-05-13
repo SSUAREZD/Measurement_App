a554a7aea721d4984d942fcc446ffeabbb0eb5ce
Andres Lamprea <a.lampreal@uniandes.edu.co>
2026-05-13 10:42:53 -0500

    Test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-const host = typeof window !== 'undefined' ? window.location.host : 'localhost:4000';
+const host = typeof window !== 'undefined' ? window.location.host : '3.134.98.147:4202';
 const protocol = typeof window !== 'undefined' ? window.location.protocol : 'https:';
 
 export const environment = {
   production: true,
   apiUrl: '/api',
   appUrl: `${protocol}//${host}`,
-  recommendationAppUrl: '/login',
+  recommendationAppUrl: 'http://3.134.98.147:4201/login',
 };
